b5cd9310e84d8d4deefc690a42d87f14ae6f3acd
coldboy002 <coldboy002@gmail.com>
2025-11-12 08:42:35 +0700

    ♻️ refactor: refactor package.json and index.js file

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 module.exports = {
-  // * https://prettier.io/docs/en/options.html
+  // https://prettier.io/docs/en/options.html
   plugins: ['@prettier/plugin-pug'],
   singleQuote: true,
   trailingComma: 'all',
 
-  // * https://github.com/prettier/plugin-pug#options
-  pugBracketSameLine: false,
-  pugAttributeSeparator: 'always',
-  pugCommentPreserveSpaces: 'keep-all',
-  pugSortAttributes: 'as-is',
-  pugEmptyAttributes: 'as-is',
-  pugClassNotation: 'literal',
-  pugIdNotation: 'literal',
-  pugClassLocation: 'before-attributes',
-  pugExplicitDiv: false,
-  pugPreserveAttributeBrackets: false,
-  pugPreserveWhitespace: true,
+  // Overrides for specific files .pug
+  overrides: [
+    {
+      files: ['*.pug'],
+      options: {
+        // See: https://github.com/prettier/plugin-pug#options
+        pugBracketSameLine: false,
+        pugAttributeSeparator: 'always',
+        pugCommentPreserveSpaces: 'keep-all',
+        pugSortAttributes: 'as-is',
+        pugEmptyAttributes: 'as-is',
+        pugClassNotation: 'literal',
+        pugIdNotation: 'literal',
+        pugClassLocation: 'before-attributes',
+        pugExplicitDiv: false,
+        pugPreserveAttributeBrackets: false,
+        pugPreserveWhitespace: true,
+      },
+    },
+  ],
 
   // Overrides for specific files .css, .sass, .scss
   overrides: [
